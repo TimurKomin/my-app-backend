48e3c094f366609b900c9044623857dfa9ca3a51
OEM Configuration (temporary user) <timurkomin9@gmail.com>
2022-06-07 17:48:12 +0300

    update patch route

--- routes/patchTask.js
+++ routes/patchTask.js
@@ -8,7 +8,7 @@ router.patch("/", async (req, res, next) => {
         const tasksList = await fs.readFile(`./server/arr.json`);
     const arrayTasks = JSON.parse(tasksList);
     if (arrayTasks.tasks.some((item) => item.name === req.body.name)) {
-        throw new ERROR(404, "Task alredy exist");
+        throw new ERROR(405, "Task alredy exist");
     }
 
     arrayTasks.tasks.map((item) => {
