e9195b96bca7e27bcc0047bd18df060e45480a4a
OEM Configuration (temporary user) <timurkomin9@gmail.com>
2022-06-09 20:10:30 +0300

    ssl

--- models/index.js
+++ models/index.js
@@ -18,14 +18,11 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    console.log("file1", file);
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    console.log("file", file);
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
-    console.log("model", model.name);
   });
 
 Object.keys(db).forEach(modelName => {
