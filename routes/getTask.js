0a1cf1d0a5ddd0627963426fcfaf613fca9448d6
OEM Configuration (temporary user) <timurkomin9@gmail.com>
2022-06-10 20:04:23 +0300

    debug routes GetTask

--- routes/getTask.js
+++ routes/getTask.js
@@ -16,60 +16,16 @@ router.get('/', async (req, res, next)  => {
         if(filterBy !== '') {
             filterBy === 'done'? a = true : a = false
         }
+        console.log(filterBy.length, a);
         const FilterTasks = await Task.findAndCountAll({
             
-            where:{
-                done:a
-            },
+            where: filterBy.length ? { done: a } : {},
             order: [['createdAt', `${order}`]],
             offset: (page) * 5 , 
             limit: allPerPage,
 
         })
-        // if(filterBy === ''){
-        //     arrFilterTasks = await Task.findAll({
-        //         order: [['createdAt', `${order}`]],
-        //         offset: (page) * 5 , 
-        //         limit: allPerPage,
-        //     })
-        //     counter = await Task.findAndCountAll();
-        //     console.log(counter.count)
-        // }
-        // if(filterBy === 'done'){
-        //     arrFilterTasks = await Task.findAll({
-        //         where:{
-        //             done:filterBy
-        //         },
-        //         order: [['createdAt', `${order}`]],
-        //         offset: (page) * 5 , 
-        //         limit: allPerPage,
-        //     })
-
-        //     counter = await Task.findAndCountAll({
-        //         where: {
-        //             done:true
-        //         }
-        //     });
-        // }
-        // if(filterBy === 'undone'){
-        //     arrFilterTasks = await Task.findAll({
-        //         where:{
-        //             done:false
-        //         },
-        //         order: [['createdAt', `${order}`]],
-        //          offset: (page) * 5 , 
-        //         limit: allPerPage,
-                
-        //     })
-        //     counter = await Task.findAndCountAll({
-        //         where: {
-        //             done:false
-        //         }
-        //     });
-        // }
         
-        // const countTasks = counter.count
-        // console.log((await counter).count)
         await res.status(200).json(FilterTasks)
     }
     catch(FilterTasks){
