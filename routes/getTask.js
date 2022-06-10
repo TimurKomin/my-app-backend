834979f5a8edc4e4131c519a29241215e9b5f848
OEM Configuration (temporary user) <timurkomin9@gmail.com>
2022-06-10 19:10:23 +0300

    debug routes

--- routes/getTask.js
+++ routes/getTask.js
@@ -12,54 +12,65 @@ router.get('/', async (req, res, next)  => {
         let { allPerPage, filterBy, page, order} = req.query
         let arrFilterTasks = []
         let counter = {}
-        if(filterBy === ''){
-            arrFilterTasks = await Task.findAll({
-                order: [['createdAt', `${order}`]],
-                offset: (page) * 5 , 
-                limit: allPerPage,
-            })
-            counter = await Task.findAndCountAll();
-            console.log(counter.count)
-        }
-        if(filterBy === 'done'){
-            arrFilterTasks = await Task.findAll({
-                where:{
-                    done:true
-                },
-                order: [['createdAt', `${order}`]],
-                offset: (page) * 5 , 
-                limit: allPerPage,
-            })
 
-            counter = await Task.findAndCountAll({
-                where: {
-                    done:true
-                }
-            });
-        }
-        if(filterBy === 'undone'){
-            arrFilterTasks = await Task.findAll({
-                where:{
-                    done:false
-                },
-                order: [['createdAt', `${order}`]],
-                 offset: (page) * 5 , 
-                limit: allPerPage,
+        const FilterTasks = await Task.findAndCountAll({
+            
+            // where:{
+            //     done:true
+            // },
+            order: [['createdAt', `${order}`]],
+            offset: (page) * 5 , 
+            limit: allPerPage,
+
+        })
+        // if(filterBy === ''){
+        //     arrFilterTasks = await Task.findAll({
+        //         order: [['createdAt', `${order}`]],
+        //         offset: (page) * 5 , 
+        //         limit: allPerPage,
+        //     })
+        //     counter = await Task.findAndCountAll();
+        //     console.log(counter.count)
+        // }
+        // if(filterBy === 'done'){
+        //     arrFilterTasks = await Task.findAll({
+        //         where:{
+        //             done:filterBy
+        //         },
+        //         order: [['createdAt', `${order}`]],
+        //         offset: (page) * 5 , 
+        //         limit: allPerPage,
+        //     })
+
+        //     counter = await Task.findAndCountAll({
+        //         where: {
+        //             done:true
+        //         }
+        //     });
+        // }
+        // if(filterBy === 'undone'){
+        //     arrFilterTasks = await Task.findAll({
+        //         where:{
+        //             done:false
+        //         },
+        //         order: [['createdAt', `${order}`]],
+        //          offset: (page) * 5 , 
+        //         limit: allPerPage,
                 
-            })
-            counter = await Task.findAndCountAll({
-                where: {
-                    done:false
-                }
-            });
-        }
+        //     })
+        //     counter = await Task.findAndCountAll({
+        //         where: {
+        //             done:false
+        //         }
+        //     });
+        // }
         
-        const countTasks = counter.count
-        console.log((await counter).count)
-        await res.status(200).json({arrFilterTasks, countTasks})
+        // const countTasks = counter.count
+        // console.log((await counter).count)
+        await res.status(200).json(FilterTasks)
     }
-    catch(counter){
-        await res.status(400).json(counter.count)
+    catch(FilterTasks){
+        await res.status(400).json(FilterTasks)
         // next(err)
     }
     
